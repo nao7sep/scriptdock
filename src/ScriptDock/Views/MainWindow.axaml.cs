86df0a06e78ba9b5370cc6c90a8e5b09cbf7130c
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2026-09-08 18:10:08 +0900

    Preserve accepted placement on close

--- src/ScriptDock/Views/MainWindow.axaml.cs
+++ src/ScriptDock/Views/MainWindow.axaml.cs
@@ -249,11 +249,6 @@ public partial class MainWindow : Window
     private void FlushWindowPlacement()
     {
         _placementSaveTimer.Stop();
-        if (_placementCaptureEnabled && !_placementTransient && WindowState == WindowState.Normal)
-        {
-            CacheCurrentNormalBounds();
-            _stableWindowMode = "normal";
-        }
         PersistWindowPlacement();
     }
 
